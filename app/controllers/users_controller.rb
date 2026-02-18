43e407c254728d508eda3ad748eb08a204fff6a3
Ryuto Sato <82396155+ryutomax@users.noreply.github.com>
2026-02-18 15:28:29 +0000

    feat: redirect_to

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,31 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-  def new   
+
+  def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)    # 実装は終わっていないことに注意!
+    if @user.save
+      redirect_to @user
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+
+      # params.expect(
+      #   user: [
+      #     :name,
+      #     :email,
+      #     :password,
+      #     :password_confirmation
+      #   ]
+      # )
+    end
 end
